15141c4b5df40c4217774635528cb78b7ef17167
kamba tukebele <kambatukebele@gmail.com>
2024-07-31 18:04:48 +0200

    updating11: updating contact section

--- src/components/contact/ContactInfo.jsx
+++ src/components/contact/ContactInfo.jsx
@@ -8,51 +8,51 @@ import Paragraph from "../Paragraph";
 import H4 from "../H4";
 const ContactInfo = () => {
   return (
-    <div className="hidden lg:flex lg:justify-center lg:flex-col lg:items-center lg:gap-4 lg:p-10 lg:w-full lg:bg-red-700 lg:rounded-2xl">
-      <div className="lg:flex lg:flex-col lg:justify-center lg:items-start lg:gap-4">
+    <div className="flex justify-center flex-col items-center gap-4 py-10 px-4 w-full bg-red-700 rounded-2xl">
+      <div className="w-full flex flex-col justify-center items-center gap-4">
         <H4
           title="Get in Touch for Expert Advisory"
-          h4ExtraClass="lg:text-white lg:font-semibold"
+          h4ExtraClass="text-white font-semibold text-center"
         />
         <Paragraph
           title="I’m here to help you with tailored solutions and strategic advice for your legal, financial, and investment needs. Whether you have questions, need a consultation, or want to discuss how I can assist your business, please reach out."
-          text_color="text-white"
+          text_color="text-white text-center lg:max-w-[800px]"
         />
       </div>
-      <div className="lg:w-full lg:flex lg:flex-col lg:justify-center lg:items-start lg:gap-4">
-        <div className="lg:flex lg:flex-col lg:justify-center lg:items-start lg:gap-2 lg:py-2">
-          <span className="lg:flex lg:justify-center lg:items-center lg:w-9 lg:h-9 lg:bg-purple50 lg:rounded-full">
+      <div className="w-full flex flex-col justify-center items-center gap-4">
+        <div className="flex flex-col justify-center items-center gap-2 py-2">
+          <span className="flex justify-center items-center w-9 h-9 bg-purple50 rounded-full">
             <FontAwesomeIcon
               className="text-white text-base"
               icon={faLocation}
             />
           </span>
           <div>
-            <span className="lg:text-white lg:block lg:font-medium lg:text-base lg:mb-1">
+            <span className="text-white block font-medium text-base mb-1 text-center">
               International House, 64 Nile Street, Londres, Royaume-Uni, N1 7SR
             </span>
           </div>
         </div>
-        <div className="lg:flex lg:flex-col lg:justify-center lg:items-start lg:gap-2 lg:py-2">
-          <span className="lg:flex lg:justify-center lg:items-center lg:w-9 lg:h-9 lg:bg-purple50 lg:rounded-full">
+        <div className="w-full flex flex-col justify-center items-center gap-2 py-2">
+          <span className="flex justify-center items-center w-9 h-9 bg-purple50 rounded-full">
             <FontAwesomeIcon className="text-white text-base" icon={faPhone} />
           </span>
           <div>
-            <p className="lg:text-white lg:block lg:font-medium lg:text-base lg:mb-1">
+            <p className="text-white block font-medium text-base mb-1">
               <span className="block">+44 741 834 4386</span>
               <span className="block">+1 252 776 4643</span>
             </p>
           </div>
         </div>
-        <div className="lg:flex lg:flex-col lg:justify-center lg:items-start lg:gap-2 lg:py-2">
-          <span className="lg:flex lg:justify-center lg:items-center lg:w-9 lg:h-9 lg:bg-purple50 lg:rounded-full">
+        <div className="w-full flex flex-col justify-center items-center gap-2 py-2">
+          <span className="flex justify-center items-center w-9 h-9 bg-purple50 rounded-full">
             <FontAwesomeIcon
               className="text-white text-base"
               icon={faEnvelope}
             />
           </span>
           <div>
-            <span className="lg:text-white lg:block lg:font-medium lg:text-base lg:mb-1">
+            <span className="text-white block font-medium text-base mb-1 text-center">
               nauvidbenza@benzagroupe.com
             </span>
           </div>
